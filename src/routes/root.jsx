4ecec2f31a3b6873699ef49d7cfdbe1375faaaff
Michaela Bucatariu <mbucatariu24@gmail.com>
2024-10-18 08:42:46 +0200

    add search

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,14 +4,19 @@ import {
   useLoaderData, 
   Form,
   redirect, 
+  useSubmit,
 } from "react-router-dom";
+import { useEffect } from "react";
 import { getContacts, createContact } from "../contacts";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+
   console.log("loader", contacts);
   console.log("loader", {contacts});
-  return { contacts };
+  return { contacts, q };
 }
 
 export async function action() {
@@ -22,33 +27,50 @@ export async function action() {
 
 export default function Root() {
 
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     console.log("Root", contacts);
     console.log(contacts.length);
+
+    const submit = useSubmit();
+
+    const searching = navigation.location &&
+        new URLSearchParams(navigation.location.search).has("q");
+
+    useEffect(() => {
+      document.getElementById("q").value = q;
+    }, [q]);
 
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                defaultValue={q}
+                onChange={(event) => {
+                  const isFirstSearch = q == null;
+                  submit(event.currentTarget.form, {
+                    replace: !isFirstSearch,
+                  });
+                }}
               />
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
